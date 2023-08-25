4c1f2bcc4adf42aeb3dd3cf7bd80fdd476ade427
Mayank Bisht <87826676+M-Bisht@users.noreply.github.com>
2023-08-25 13:47:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const apiKey =
-  "70fe5f8b7873c2a5c92de4198d2e0d148b686b6283ada675ebd80c55316db6a4fcc42b84c3c19db959265ba5539e084d";
+  "bcd6862ca98aa1a0ac7eb562a60356db32c9ec16f36f4beede75e2b8df26142b070bc2cbd9ca88d694e51a1e72c35989";
 const imageInput = document.getElementById("imageInput");
 const uploadButton = document.getElementById("uploadButton");
 const processedImage = document.getElementById("processedImage");
